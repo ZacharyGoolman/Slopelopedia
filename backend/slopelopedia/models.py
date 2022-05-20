a33f92a2e8433ee4ed7b79d87f5dbccfe52a2d45
ZacharyGoolman <zachary.goolman@gmail.com>
2022-05-20 13:37:31 -0400

    Added post field for ERD + Created models in line with ERD + Made serializers file and created serializers for my Models

--- backend/slopelopedia/models.py
+++ backend/slopelopedia/models.py
@@ -4,16 +4,15 @@ from django.db import models
 from django.db import models
 from authentication.models import User 
 
-
-
-
-
-
-
-
-
-
-
+class Location(models.Model):
+    location = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    weather = models.
+    difficulty = models.CharField(max_length=20)
+    post = models.TextField(max_length=5000)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ backend/slopelopedia/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Comment 
+from .models import Reply
+from .models import Location
+
+
+class LocationSerializer(serializers.LocationSerializer):
+    class Meta:
+        model = Location
+        fields = ['location', 'latitude', 'longitude', 'weather', 'post', 'difficulty', 'likes', 'dislikes']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['user','video_id','text','likes','dislikes']
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['id','user','comment_id','comment','text']
+        depth = 1
+    comment_id = serializers.CharField(write_only=True)
