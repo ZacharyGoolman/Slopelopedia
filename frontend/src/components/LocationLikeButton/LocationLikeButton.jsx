4a3b095f9e92b6ebfb807f1f1d1557958a947079
ZacharyGoolman <zachary.goolman@gmail.com>
2022-05-26 16:01:57 -0400

    Added style sheets for like and dislike button + created the universal button functionality and added some styling

--- frontend/src/components/LocationLikeButton/LocationLikeButton.jsx
+++ frontend/src/components/LocationLikeButton/LocationLikeButton.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import cn from "classnames";
 import { ReactComponent as Hand } from "./hand.svg";
-import "./LocationDisLikesButton.css";
+import "./LocationLikesButton.css";
 import "./styles.scss";
 
-const DisLikeButton = () => {
-  const [Disliked, setDisLiked] = useState(null);
+const LikeButton = () => {
+  const [liked, setLiked] = useState(null);
   const [clicked, setClicked] = useState(false);
 
   return (
     <button
       onClick={() => {
-        setDisLiked(!Disliked);
+        setLiked(!liked);
         setClicked(true);
       }}
       onAnimationEnd={() => setClicked(false)}
       className={cn("like-button-wrapper", {
-        Disliked,
+        liked,
         clicked,
       })}
     >
       <div className="like-button">
         <Hand />
         <span>Like</span>
-        <span className={cn("suffix", { Disliked })}>d</span>
+        <span className={cn("suffix", { liked })}>d</span>
       </div>
     </button>
   );
 };
 
-export default DisLikeButton;
+export default LikeButton;
